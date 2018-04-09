1e469a4f08df34fdd9da2fe60b4a4fb781dd1451
akseart <akseart@ya.ru>
2018-04-09 15:26:16 +0300

    Initial Commit

---
+++ interpolation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interpolation
+//
+//  Created by Артем on 09.04.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
